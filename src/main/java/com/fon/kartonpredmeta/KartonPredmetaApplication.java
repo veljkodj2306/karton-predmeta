77aa0b4a3491da6c9920b18423b34ee8ee2ecd41
Veljko <veljkodj2306@gmail.com>
2026-02-09 15:33:47 +0100

    Add Spring Boot initial project

---
+++ src/main/java/com/fon/kartonpredmeta/KartonPredmetaApplication.java
@@ -0,0 +1,13 @@
+package com.fon.kartonpredmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartonPredmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartonPredmetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fon/kartonpredmeta/KartonPredmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fon.kartonpredmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartonPredmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
